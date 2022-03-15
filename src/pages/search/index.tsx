4d47588af7f24aff086aafe7145c19595e5e4475
William Takashi Mimura <william.mimura@thrive.health>
2022-03-15 01:01:11 -0700

    Add search page

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,9 +1,27 @@
+import { Box, Container } from '@mui/material';
 import React from 'react';
+import { MainContainer } from '../../components/Container/MainContainer';
+import { LinkButton } from '../../components/Link/Link';
+import { ThemeSwitcher } from '../../components/ThemeSwitcher/ThemeSwitcher';
+import { Body1, Heading1, Heading2 } from '../../components/Typography/Typography';
+import { useTitle } from '../../utils/useTitle';
 
-const Search: React.FC<SearchProps> = () => {
-  return <div>hello :)</div>;
+const SearchPage: React.FC = () => {
+  useTitle('Search');
+
+  return (
+    <MainContainer>
+      <ThemeSwitcher />
+      <Container>
+        <Heading1>GitHub Search API</Heading1>
+        <Box mt={3} mb={3}>
+          <LinkButton to='/' text='Go back' />
+        </Box>
+        <Heading2>User search</Heading2>
+        <Body1>Hehe :)</Body1>
+      </Container>
+    </MainContainer>
+  );
 };
 
-interface SearchProps {}
-
-export default Search;
+export default SearchPage;
